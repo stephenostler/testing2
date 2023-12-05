e78b19678925b89752cef3d133a2e3dc2d1427ea
StephenOstler <stephen.ostler0120@stu.mtec.edu>
2023-12-05 12:10:57 -0700

    commit for adding in some checkout code

--- pageobjects/checkout.js
+++ pageobjects/checkout.js
@@ -17,15 +17,56 @@ class Checkout extends Page {
        return $('[data-test="checkout"]');
    }
 
-   
-   get butcheck2(){
+   get postalcode(){
        return $('#postal-code');
+   }
+
+   get firstNamecheckout (){
+        return $('#first-name');
+   }
+
+   get lastNamecheckout () {
+        return $('#last-name');
+   }
+
+   get continueBtn (){
+        return $('#continue')
+   }
+
+   get finishBtn (){
+        return $('#finish')
+   }
+
+   get backHomeBtn (){
+        return $('#back-to-products')
+   }
+
+   async continueClick(){
+        await this.continueBtn.click
+   }
+
+   async finishClick(){
+        await this.finishBtn.click
+   }
+
+   async homeClick(){
+        await this.backHomeBtn.click
+   }
+
+   async userInfo (username, lastname, zipcode){
+        await this.firstNamecheckout.setValue(username)
+        await this.lastNamecheckout.setValue(lastname)
+        await this.postalcode.setValue(zipcode)
    }
 
    async checkoutTotal (){
         await this.cartClick();
         await this.checkoutClick();
-        await expect(this.butcheck2).toBeExisting();
+        await expect(this.postalcode).toBeExisting();
+        await this.userInfo('meg', 'griffin', '45678');
+        await this.continueClick();
+        await this.finishClick();
+        await this.homeClick();
    }
 }
 
